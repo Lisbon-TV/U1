ca4941086dfc21b4ea2cc4adad76050f48b16615
Lisbon <lisbon@dublin.com>
2023-07-31 10:54:47 +0300

    LocalStorage -addition with a list

--- u1/src/App.jsx
+++ u1/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import { store } from "./Functions/LocalStorage";
+import { get, store } from "./Functions/LocalStorage";
 
 const KEY = "persons";
 
@@ -10,6 +9,7 @@ function App() {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [persons, setPersons] = useState(null);
+  const [lastUpdate, setLastUpdate] = useState(Date.now());
 
   const handleChangeName = (e) => {
     setName(e.target.value);
@@ -28,14 +28,22 @@ function App() {
     store(KEY, person);
     setLastName("");
     setName("");
+    setLastUpdate(Date.now());
   };
+
+  useEffect(
+    (_) => {
+      setPersons(get(KEY));
+    },
+    [lastUpdate]
+  );
 
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col-4">
           <div className="card mt-4">
-            <h3 className="card-header">Add New Client</h3>
+            <h3 className="card-header">Add New Account Holder:</h3>
             <div className="card-body">
               <fieldset>
                 <legend>First Name</legend>
@@ -55,9 +63,48 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="col-8 mt-4">Column</div>
+        <div className="col-8 mt-4">
+          <div className="card">
+            <h1 className="card-header">Account Holders</h1>
+            <div className="card-body">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Ballance</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {persons &&
+                    persons.map((person) => (
+                      <tr key={person.id}>
+                        <td>{person.name}</td>
+                        <td>{person.lastName}</td>
+                        <td>{person.money}</td>
+                        <td>
+                          <button>Delete</button>
+                        </td>
+                        <td>
+                          <div>
+                            <input type="number" />
+                          </div>
+                        </td>
+                        <td>
+                          <button>Edit</button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
 export default App;
